55b4510c068c6d8417b3295c8a361ab708e2c246
Ting <ting294@hotmail.com>
2025-07-10 07:47:09 +0800

    Update keymap: Optimize with AML
    1. Restore mouse layer back to 1: Make nav and higher layer not blocked by AML timeout
    2. Optimize Fn layer
    3. Add Media control
    4. Add Editor buttons on nav and mouse layer

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via/config.h
@@ -36,6 +36,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TAP_CODE_DELAY 5
 
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
-#define AUTO_MOUSE_DEFAULT_LAYER 3
+#define AUTO_MOUSE_DEFAULT_LAYER 1
 #define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
 #define DYNAMIC_KEYMAP_LAYER_COUNT 6
--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via/keymap.c
@@ -20,46 +20,54 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "quantum.h"
 
-#define SCROLL_LAYER 4 // must greater than AUTO_MOUSE_DEFAULT_LAYER
+#define SCROLL_LAYER 2 // must greater than AUTO_MOUSE_DEFAULT_LAYER
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // keymap for default (VIA)
   [0] = LAYOUT_universal(
-    KC_ESC   , KC_Q         ,    KC_W     ,     KC_E    ,    KC_R     , KC_T     ,                                        KC_Y     ,     KC_U     ,     KC_I     ,     KC_O     ,        KC_P     , KC_DEL   ,
+    KC_ESC   , KC_Q         ,    KC_W     ,     KC_E    ,    KC_R     , KC_T     ,                                        KC_Y     ,     KC_U     ,     KC_I     ,     KC_O     ,        KC_P     , G(KC_L)   ,
     KC_TAB   , LGUI_T(KC_A) , LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G     ,                                        KC_H     , RSFT_T(KC_J) , RCTL_T(KC_K) , RALT_T(KC_L) , RGUI_T(KC_SCLN) , S(KC_7)  ,
-    KC_LSFT  , KC_Z         ,    KC_X     ,     KC_C    ,    KC_V     , KC_B     ,                                        KC_N     ,     KC_M     ,     KC_COMM  ,     KC_DOT   ,        KC_SLSH  , KC_INT1  ,
-                                  KC_LALT,KC_LGUI,LT(4,KC_ESC)  ,LT(1,KC_SPC),    LT(3,KC_TAB),                  KC_BSPC,LT(2,KC_ENT), RCTL_T(KC_LNG2),     KC_RALT  , KC_PSCR
+    KC_LSFT  , KC_Z         ,    KC_X     ,     KC_C    ,    KC_V     , KC_B     ,                                        KC_N     ,     KC_M     ,     KC_COMM  ,     KC_DOT   ,        KC_SLSH  , G(KC_SPC)  ,
+                                  KC_LALT,KC_LGUI,LT(4,KC_ESC)  ,LT(2,KC_SPC),    LT(1,KC_TAB),                  LT(5,KC_BSPC),LT(3,KC_ENT), RCTL_T(KC_LNG2),     KC_RALT  , KC_PSCR
+  ),
+  
+  //mouse
+  [1] = LAYOUT_universal(
+    _______  , _______  , _______  , _______  , _______  , _______  ,                                        _______  , C(KC_V)  , C(KC_C)  , C(KC_X)  , C(KC_Z)  , _______  ,
+    _______  , KC_LGUI  , KC_LALT  , KC_LCTL  , KC_LSFT  , _______  ,                                        _______  , KC_BTN1  , KC_BTN3  , KC_BTN2  , _______  , _______  ,
+    _______  , C(KC_Z)  , C(KC_X)  , C(KC_C)  , C(KC_V)  , _______  ,                                        _______  , KC_BTN4  , KC_BTN5  , _______  , _______  , _______  ,
+                  _______  , _______ , _______  ,         _______  , _______  ,                    _______  , _______  , _______       , _______  , _______
   ),
 
   // Nav // TBD
-  [1] = LAYOUT_universal(
-    _______  , _______  , _______  , _______  , _______  , _______  ,                                         _______  , _______  , _______  , _______  , _______  , _______  ,
-    _______  , _______  , _______  , _______  , _______  , _______  ,                                         KC_LEFT  , KC_DOWN  , KC_UP    , KC_RGHT  , _______  , _______  ,
-    _______  , _______  , _______  , _______  , _______  , _______  ,                                         KC_HOME  , KC_PGDN  , KC_PGUP  , KC_END   , _______  , _______  ,
+  [2] = LAYOUT_universal(
+    _______  , _______  , _______  , _______  , _______  , _______  ,                                         _______  , C(KC_V)  , C(KC_C)  , C(KC_X)  , C(KC_Z)  , _______  ,
+    _______  , KC_LGUI  , KC_LALT  , KC_LCTL  , KC_LSFT  , _______  ,                                         KC_LEFT  , KC_DOWN  , KC_UP    , KC_RGHT  , _______  , _______  ,
+    _______  , C(KC_Z)  , C(KC_X)  , C(KC_C)  , C(KC_V)  , _______  ,                                         KC_HOME  , KC_PGUP  , KC_PGDN  , KC_END   , _______  , _______  ,
                   _______  , _______ , _______  ,         _______  , _______  ,                   KC_DEL   , _______  , _______       , _______  , _______
   ),
 
   // symbol and num // TBD
-  [2] = LAYOUT_universal(
-    _______  ,S(KC_QUOT), KC_7     , KC_8    , KC_9     , S(KC_8)  ,                                         S(KC_7)  , S(KC_LBRC), S(KC_RBRC), S(KC_3)  , S(KC_4)  , _______  ,
+  [3] = LAYOUT_universal(
+    _______  , KC_GRV   , KC_7     , KC_8    , KC_9     , S(KC_8)  ,                                         S(KC_7)  , S(KC_LBRC), S(KC_RBRC), S(KC_3)  , S(KC_4)  , _______  ,
     _______  ,S(KC_SCLN), KC_4     , KC_5    , KC_6     , KC_MINS  ,                                         KC_BSLS  , S(KC_9)   , S(KC_0)   , KC_EQL   , KC_QUOT  , S(KC_2)  ,
-    _______  ,S(KC_MINS), KC_1     , KC_2    , KC_3     ,S(KC_EQL),                                        S(KC_BSLS) , KC_LBRC   , KC_RBRC   ,S(KC_1)   ,S(KC_SLSH),S(KC_INT3),
-                  KC_0     , KC_DOT  , _______  ,         _______  , _______  ,                   KC_DEL   , _______  , _______       , _______  , _______
+    _______  ,S(KC_MINS), KC_1     , KC_2    , KC_3     , S(KC_EQL),                                       S(KC_BSLS) , KC_LBRC   , KC_RBRC   ,S(KC_1)   ,S(KC_SLSH),S(KC_INT3),
+                  _______  , _______ , KC_DOT  ,          KC_0     , _______  ,                   KC_DEL   , _______  , _______       , _______  , _______
   ),
 
-  // mouse and fn
-  [3] = LAYOUT_universal(
-    _______  ,   KC_F1   , KC_F2    , KC_F3   , KC_F4    , KC_F5    ,                                        _______  , _______  , _______  , _______  , _______  , _______  ,
-    _______  ,   KC_F6   , KC_F7    , KC_F8   , KC_F9    , KC_F10   ,                                        _______  , KC_BTN1  , KC_BTN3  , KC_BTN2  , _______  , _______  ,
-    _______  ,   KC_F11  , KC_F12   , _______ , _______  , _______  ,                                        _______  , KC_BTN4  , KC_BTN5  , _______  , _______  , KBC_SAVE ,
-                  _______  , _______  , _______  ,        _______  , _______  ,                   _______  , _______  , _______       , _______  , _______
+  // fn and media
+  [4] = LAYOUT_universal(
+    _______  ,   KC_F1   , KC_F2    , KC_F3   , KC_F4    , _______  ,                                        _______  , _______  , _______  , _______  , _______  , _______  ,
+    _______  ,   KC_F5   , KC_F6    , KC_F7   , KC_F8    , _______  ,                                        _______  , KC_MPRV  , KC_VOLD  , KC_VOLU  , KC_MNXT  , _______  ,
+    _______  ,   KC_F9   , KC_F10   , KC_F11  , KC_F12   , _______  ,                                        _______  , _______  , _______  , _______  , _______  , _______  ,
+                  _______  , _______  , _______  ,        _______  , _______  ,                   _______  , KC_MPLY  , _______       , _______  , _______
   ),
 
   // config layer
-  [4] = LAYOUT_universal(
+  [5] = LAYOUT_universal(
     _______  , _______  , _______  , _______  , _______  , _______  ,                                        AML_TO   , AML_I50  , AML_D50  , _______  , _______  , _______  ,
-    _______  , _______  , _______  , _______  , _______  , _______  ,                                        SCRL_DVD , SCRL_DVI , _______  , _______  , _______  , _______  ,
+    _______  , _______  , _______  , _______  , _______  , _______  ,                                        _______  , SCRL_DVD , SCRL_DVI , _______  , _______  , _______  ,
     _______  , _______  , _______  , _______  , _______  , _______  ,                                        CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , _______  , KBC_SAVE ,
                   QK_BOOT  , KBC_RST  , _______  ,        _______  , _______  ,                   _______  , _______  , _______       , KBC_RST  , QK_BOOT
   ),
