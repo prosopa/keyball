39b07a9ef0376f6f34272df0b008dc4368f6ea8e
Ting <ting294@hotmail.com>
2026-05-12 01:52:46 +0800

    feat: Update keymap - colemak-dh layer & combo for mouse keys
    1. Add additional base layer: colemak-dh
    2. Add combo keys: for mouse key

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via/config.h
@@ -37,7 +37,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define FLOW_TAP_TERM 150
 
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
-#define AUTO_MOUSE_DEFAULT_LAYER 1
+#define AUTO_MOUSE_DEFAULT_LAYER 2
 #define AUTO_MOUSE_DELAY 200
 #define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
-#define DYNAMIC_KEYMAP_LAYER_COUNT 7
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
+#define COMBO_ONLY_FROM_LAYER 0
--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via/keymap.c
@@ -20,7 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "quantum.h"
 
-#define SCROLL_LAYER 2 // must greater than AUTO_MOUSE_DEFAULT_LAYER
+#define SCROLL_LAYER 3 // must greater than AUTO_MOUSE_DEFAULT_LAYER
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -29,11 +29,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC   , KC_Q         ,    KC_W     ,     KC_E    ,    KC_R     , KC_T     ,                                        KC_Y     ,     KC_U     ,     KC_I     ,     KC_O     ,        KC_P     , G(KC_L)   ,
     KC_TAB   , LGUI_T(KC_A) , LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G     ,                                        KC_H     , RSFT_T(KC_J) , RCTL_T(KC_K) , RALT_T(KC_L) , RGUI_T(KC_SCLN) , S(KC_7)  ,
     KC_LSFT  , KC_Z         ,    KC_X     ,     KC_C    ,    KC_V     , KC_B     ,                                        KC_N     ,     KC_M     ,     KC_COMM  ,     KC_DOT   ,        KC_SLSH  , G(KC_SPC)  ,
-                                  KC_LALT,KC_LGUI,LT(1,KC_ESC)  ,LT(2,KC_SPC),    LT(4,KC_TAB),                  LT(5,KC_BSPC),LT(3,KC_ENT), RCTL_T(KC_LNG2),     KC_RALT  , KC_PSCR
+                                  KC_LALT,KC_LGUI,LT(2,KC_ESC)  ,LT(3,KC_SPC),    LT(5,KC_TAB),                  LT(6,KC_BSPC),LT(4,KC_ENT), RCTL_T(KC_LNG2),     KC_RALT  , KC_PSCR
+  ),
+
+  // colemak-dh
+  [1] = LAYOUT_universal(
+    KC_ESC   , KC_Q         ,    KC_W     ,     KC_F    ,    KC_P     , KC_B     ,                                        KC_J     ,     KC_L     ,     KC_U     ,     KC_Y     ,     KC_SCLN     , G(KC_L)   ,
+    KC_TAB   , LGUI_T(KC_A) , LALT_T(KC_R), LCTL_T(KC_S), LSFT_T(KC_T), KC_G     ,                                        KC_M     , RSFT_T(KC_N) , RCTL_T(KC_E) , RALT_T(KC_I) , RGUI_T(KC_O)    , S(KC_7)  ,
+    KC_LSFT  , KC_Z         ,    KC_X     ,     KC_C    ,    KC_D     , KC_V     ,                                        KC_K     ,     KC_H     ,     KC_COMM  ,     KC_DOT   ,        KC_SLSH  , G(KC_SPC)  ,
+                                  KC_LALT,KC_LGUI,LT(2,KC_ESC)  ,LT(3,KC_SPC),    LT(5,KC_TAB),                  LT(6,KC_BSPC),LT(4,KC_ENT), RCTL_T(KC_LNG2),     KC_RALT  , KC_PSCR
   ),
   
   //mouse
-  [1] = LAYOUT_universal(
+  [2] = LAYOUT_universal(
     _______  , _______  , _______  , _______  , _______  , _______  ,                                        _______  , C(KC_V)  , C(KC_C)  , C(KC_X)  , C(KC_Z)  , _______  ,
     _______  , KC_LGUI  , KC_LALT  , KC_LCTL  , KC_LSFT  , _______  ,                                        _______  , KC_BTN1  , KC_BTN3  , KC_BTN2  , _______  , _______  ,
     _______  , C(KC_Z)  , C(KC_X)  , C(KC_C)  , C(KC_V)  , _______  ,                                        _______  , KC_BTN4  , KC_BTN5  , _______  , _______  , _______  ,
@@ -41,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   // Nav // TBD
-  [2] = LAYOUT_universal(
+  [3] = LAYOUT_universal(
     _______  , _______  , _______  , _______  , _______  , _______  ,                                         _______  , C(KC_V)  , C(KC_C)  , C(KC_X)  , C(KC_Z)  , _______  ,
     _______  , KC_LGUI  , KC_LALT  , KC_LCTL  , KC_LSFT  , _______  ,                                         KC_LEFT  , KC_DOWN  , KC_UP    , KC_RGHT  , _______  , _______  ,
     _______  , C(KC_Z)  , C(KC_X)  , C(KC_C)  , C(KC_V)  , _______  ,                                         KC_HOME  , KC_PGUP  , KC_PGDN  , KC_END   , _______  , _______  ,
@@ -49,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   // symbol and num // TBD
-  [3] = LAYOUT_universal(
+  [4] = LAYOUT_universal(
     _______  , KC_GRV   , KC_7     , KC_8    , KC_9     , S(KC_8)  ,                                         S(KC_7)  , S(KC_LBRC), S(KC_RBRC), S(KC_3)  , S(KC_4)  , _______  ,
     _______  ,S(KC_SCLN), KC_4     , KC_5    , KC_6     , KC_MINS  ,                                         KC_BSLS  , S(KC_9)   , S(KC_0)   , KC_EQL   , KC_QUOT  , S(KC_2)  ,
     _______  ,S(KC_MINS), KC_1     , KC_2    , KC_3     , S(KC_EQL),                                       S(KC_BSLS) , KC_LBRC   , KC_RBRC   ,S(KC_1)   ,S(KC_SLSH),S(KC_INT3),
@@ -57,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   // fn and media
-  [4] = LAYOUT_universal(
+  [5] = LAYOUT_universal(
     _______  ,   KC_F1   , KC_F2    , KC_F3   , KC_F4    , _______  ,                                        _______  , _______  , _______  , _______  , _______  , _______  ,
     _______  ,   KC_F5   , KC_F6    , KC_F7   , KC_F8    , _______  ,                                        _______  , KC_MPRV  , KC_VOLD  , KC_VOLU  , KC_MNXT  , _______  ,
     _______  ,   KC_F9   , KC_F10   , KC_F11  , KC_F12   , _______  ,                                        _______  , _______  , _______  , _______  , _______  , _______  ,
@@ -65,22 +73,44 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   // config layer
-  [5] = LAYOUT_universal(
+  [6] = LAYOUT_universal(
     _______  , _______  , _______  , _______  , _______  , _______  ,                                        AML_TO   , AML_I50  , AML_D50  , _______  , _______  , _______  ,
-    _______  , _______  , _______  , _______  , _______  , _______  ,                                        _______  , SCRL_DVD , SCRL_DVI , _______  , _______  , _______  ,
+    _______  , _______  , _______  , _______  , _______  , _______  ,                                        _______  , SCRL_DVD , SCRL_DVI , _______  , TG(1)    , _______  ,
     _______  , _______  , _______  , _______  , _______  , _______  ,                                        CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , _______  , KBC_SAVE ,
                   QK_BOOT  , KBC_RST  , _______  ,        _______  , _______  ,                   _______  , _______  , _______       , KBC_RST  , QK_BOOT
   ),
 
-  // experimental syn/num layer
-  [6] = LAYOUT_universal(
+  // experimental syn/num layer : under construction
+  [7] = LAYOUT_universal(
     _______  ,   KC_1   ,   KC_2   ,   KC_3   ,   KC_4   ,   KC_5   ,                                          KC_6   ,   KC_7   ,   KC_8   ,   KC_9   ,   KC_0   , _______  ,
-    _______  ,  S(KC_1) ,  S(KC_2) ,  S(KC_3) ,  S(KC_4) ,  S(KC_5) ,                                        KC_QUOT  ,  S(KC_9) , KC_LBRC  ,S(KC_LBRC), _______  , _______  ,
-    _______  , _______  , _______  , _______  , _______  , _______  ,                                        KC_MINS  ,  S(KC_0) , KC_RBRC  ,S(KC_RBRC), KC_BSLS  , _______  ,
+    _______  ,  S(KC_1) ,  S(KC_2) ,  S(KC_3) ,  S(KC_4) ,  S(KC_5) ,                                        KC_EQL  ,  S(KC_7) , S(KC_8)  ,  _______ , KC_QUOT  , _______  ,
+    _______  ,  KC_GRV  , S(KC_LBRC)  , KC_LBRC  ,  S(KC_9)  , _______  ,                                    KC_MINS  ,  S(KC_0) , KC_RBRC  ,S(KC_RBRC), KC_BSLS  , _______  ,
                   _______  , _______  , _______  ,        _______  , _______  ,                   _______  , _______  , _______       , _______  , _______
   )
 };
 // clang-format on
+
+enum combos {
+  JK_BTN1,
+  KL_BTN2,
+  JL_BTN3,
+  MCOM_BTN4,
+  COMDOT_BTN5
+};
+
+const uint16_t PROGMEM jk_combo[] = { KC_J, KC_K, COMBO_END };
+const uint16_t PROGMEM kl_combo[] = { KC_K, KC_L, COMBO_END };
+const uint16_t PROGMEM jl_combo[] = { KC_J, KC_L, COMBO_END };
+const uint16_t PROGMEM mcom_combo[] = { KC_M, KC_COMM, COMBO_END };
+const uint16_t PROGMEM comdot_combo[] = { KC_COMM, KC_DOT, COMBO_END };
+
+combo_t key_combos[] = {
+  [JK_BTN1] = COMBO( jk_combo, KC_BTN1 ),
+  [KL_BTN2] = COMBO( kl_combo, KC_BTN2 ),
+  [JL_BTN3] = COMBO( jl_combo, KC_BTN3 ),
+  [MCOM_BTN4] = COMBO( mcom_combo, KC_BTN4 ),
+  [COMDOT_BTN5] = COMBO( comdot_combo, KC_BTN5 ),
+};
 
 layer_state_t layer_state_set_user(layer_state_t state) {
     // Auto enable scroll mode when the highest layer is SCROLL_LAYER
