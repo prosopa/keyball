a697e44c34dd40d51e4beb0143443e9995334a6e
Ting <ting294@hotmail.com>
2026-05-16 13:38:04 +0800

    Update swapper: keep cmdish registered when one shot shift is down, not shift

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via/keymap.c
@@ -158,7 +158,7 @@ void update_swapper(
             unregister_code(tabish);
             // Don't unregister cmdish until some other key is hit or released.
         }
-    } else if (*active && !(keycode == KC_LSFT || keycode == KC_LEFT || keycode == KC_DOWN || keycode ==  KC_UP || keycode == KC_RGHT)) {
+    } else if (*active && !(keycode == OSM(MOD_LSFT) || keycode == KC_LEFT || keycode == KC_DOWN || keycode ==  KC_UP || keycode == KC_RGHT)) {
         unregister_code(cmdish);
         *active = false;
     }
